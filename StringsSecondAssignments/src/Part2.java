3fafa0500faf1945a8e25594416cf9e55f141312
KimSopagnaphea <36412763+FrostCodeBite@users.noreply.github.com>
2025-02-08 02:41:19 +0100

    StringsSecondAssignments

---
+++ StringsSecondAssignments/src/Part2.java
@@ -0,0 +1,23 @@
+public class Part2 {
+
+    public static int howMany(String stringa, String stringb) {
+
+        String tempString = stringb.replaceAll(stringa, "");
+        int count = (stringb.length() - tempString.length())/stringa.length();
+        
+        return count;
+    }
+
+    public static void testHowMany() {
+        int time = howMany("GAA", "ATGAACGAATTGAATC");
+        System.err.println(time);
+
+        int time2 = howMany("AA", "ATAAAA");
+        System.err.println(time2);
+    }
+
+    public static void main(String[] args) {
+        testHowMany();
+        
+    }
+}
